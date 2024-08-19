4533f3fbc1c687a1d9797b5c37482b9f6ea5934f
gaganB2 <105031075+gaganB2@users.noreply.github.com>
2024-08-19 11:44:10 +0530

    .

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int num1, num2;
+	printf("enter two numbers : ");
+	scanf("%d%d",&num1,&num2);
+	printf("\nsum of two numbers is : %d", num1+num2);
+	return 0;
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+scanf("%d%d",&num1,&num2);
